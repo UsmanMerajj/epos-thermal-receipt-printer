1e1b41b1d67ec26f5d3c9741f9c6f94b74bef12b
Usman Meraj <usman.meraj@iblinknext.com>
2024-10-01 01:44:27 +0500

    Added openCasher in android USB printing

--- dist/utils/EPToolkit.d.ts
+++ dist/utils/EPToolkit.d.ts
@@ -1,9 +1,10 @@
 /// <reference types="node" />
 declare type IOptions = {
-    beep: boolean;
-    cut: boolean;
-    tailingLine: boolean;
-    encoding: string;
+  beep: boolean;
+  cut: boolean;
+  tailingLine: boolean;
+  openCasher: boolean;
+  encoding: string;
 };
 export declare function exchange_text(text: string, options: IOptions): Buffer;
 export {};
